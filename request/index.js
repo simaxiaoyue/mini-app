8a8718c2b3fb7de00fde43aaa60412181441aee0
carrot cat <carrot cat@gmail.com>
2019-09-23 16:57:03 +0800

    02-发送请求的封装和loading效果

--- request/index.js
+++ request/index.js
@@ -1,4 +1,13 @@
+let requestTimes=0
 export const request = (params) => {
+    requestTimes++;
+    //设置loading效果
+    wx.showLoading({
+        title: "加载中",
+        // 遮罩层  true-> 用户无法再次点击 屏幕 
+        mask: true
+    });
+    //设置公共的url
     let baseUrl = "https://api.zbztb.cn/api/public/v1"
     return new Promise((resolve, reject) => {
         wx.request({
@@ -11,7 +20,8 @@ export const request = (params) => {
                 reject(err);
             },
             completed: () => {
-
+                requestTimes--;
+                requestTimes===0 && wx.hideLoading();
             }
         })
     })
