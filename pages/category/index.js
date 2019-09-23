514bec9d8e3b972092ea8f4ffff0c4e542a2b5e3
carrot cat <carrot cat@gmail.com>
2019-09-23 20:36:25 +0800

    03-分类页的渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,35 @@
-// pages/category/index.js
+import { request } from "../../request/index.js"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    //左侧菜单数据
+    menuList: [],
+    //右侧列表数据
+    cateList: [],
+    // 当前索引
+    currentIndex: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    this.getCateData()
   },
+  //获取分类数据
+  getCateData() {
+    request({ url: "/categories" })
+      .then(res => {
+        // console.log(res);
+        let menuList = res.data.message.map(e => e.cat_name)
+        let cateList = res.data.message[0].children
+        this.setData({
+          menuList, cateList
+        })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击菜单切换
+  changeMenu(e) {
+    const { index } = e.target.dataset
+    this.setData({
+      currentIndex:index
+    })
   }
 })
