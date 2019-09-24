0447c795280f20c7b7932b6b1894a5be4dd1debc
carrot cat <carrot cat@gmail.com>
2019-09-24 20:10:04 +0800

    06-商品列表页的下拉加载完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -7,8 +7,11 @@ Page({
       { id: 1, text: "销量" },
       { id: 2, text: "价格" }
     ],
-    goodList: []
+    goodList: [],
+    isExitData:false
   },
+  //列表总页数
+  totalPage: 1,
   //发送请求所需要的参数
   GoodQuery: {
     //关键字
@@ -18,7 +21,7 @@ Page({
     //页码
     pagenum: 1,
     //页容量
-    pagesize: 20
+    pagesize: 10
   },
   onLoad(options) {
     this.GoodQuery.cid = options.cid
@@ -32,12 +35,32 @@ Page({
       data: this.GoodQuery
     }).then(res => {
       // console.log(res);
-      let goodList = res.data.message.goods;
-      console.log(goodList);
+      let newList = res.data.message.goods;
+      let oldList = this.data.goodList;
+      //获取数据总条数
+      let total = res.data.message.total;
+      //计算总页数
+      this.totalPage = Math.ceil(total / this.GoodQuery.pagesize)
       this.setData({
-        goodList
+        goodList: [...oldList, ...newList] 
       })
-      
+
     })
+  },
+  //滚动条触底时触发
+  onReachBottom() {
+    if (this.GoodQuery.pagenum >= this.totalPage) {
+      this.setData({
+        isExitData:true
+      })
+     wx.showToast({
+       title: '已经没有下一页了',
+       icon: 'none',
+       mask: true,
+     });
+    } else {
+      this.GoodQuery.pagenum++;
+      this.getGoodData()
+    }
   }
 })
