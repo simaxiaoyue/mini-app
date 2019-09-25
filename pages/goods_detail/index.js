23fb89fdab44b360802dcbc5a0b9b85d02028fd9
carrot cat <carrot cat@gmail.com>
2019-09-25 16:30:21 +0800

    09-商品详情页底部bar的功能实现

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -29,5 +29,34 @@ Page({
             urls:urls,// 需要预览的图片http链接列表 // ["1.png"，"2.png"...]
             current: current // 当前显示图片的http链接
         })
+    },
+    //加入购物车
+    addCart(){
+        const {detailList}=this.data
+        //获取本地数据
+        let cartList=wx.getStorageSync('carts')||[]
+        //根据id判断商品是否存在
+        let index=cartList.findIndex(e=>e.goods_id===detailList.goods_id)
+        if(index=== -1){
+            //没找到商品
+            cartList.push({
+                goods_id:detailList.goods_id,
+                goods_name:detailList.goods_name,
+                goods_price:detailList.goods_price,
+                goods_small_logo:detailList.goods_small_logo,
+                num:1
+            })
+           
+        }else{
+            //如果有,取出数据,数量++
+            cartList[index].num++;
+        }
+         //存入本地
+         wx.setStorageSync('carts',cartList);
+         wx.showToast({
+             title: '加入成功',
+             icon: 'success',
+             mask: true,
+         });
     }
 })
